ad16f5f08f083cc5896ab8fd7c1fe3d1a5e28dd8
pcarpiom <pcarpiom@users.noreply.github.com>
2024-05-02 23:00:25 -0600

    to read input file

--- lj_param_gen.py
+++ lj_param_gen.py
@@ -1,1 +1,15 @@
+import argparse
 import numpy as np
+import pandas as pd 
+
+parser = argparse.ArgumentParser()
+parser.add_argument('input_file', help='enter xyz file')
+args = parser.parse_args()
+
+#read input parameters
+
+file = open(args.input_file, 'r')
+n_params = sum(1 for line in file)
+file = open(args.input_file, 'r')
+
+print(file)
