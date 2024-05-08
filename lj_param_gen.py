cbba60570b386ec73014a76781bf1ad699f90d1a
pcarpiom <pcarpiom@users.noreply.github.com>
2024-05-08 15:50:39 -0600

    input reading lines added

--- lj_param_gen.py
+++ lj_param_gen.py
@@ -4,5 +4,14 @@ import pandas as pd
 input_file = open('parameters_file.txt','r')
 lines = input_file.readlines()
 
-for l in lines:
-    print(l.split())
+index = []
+lj_type = []
+epsilon = []
+sigma = []
+
+for i in lines:
+    line = i.split()
+    index.append(int(line[0]))
+    lj_type.append(line[1])
+    epsilon.append(float(line[2]))
+    sigma.append(float(line[3]))
