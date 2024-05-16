72a1d6fec418cc1be4eae500939b9297c7cb9f59
pcarpiom <pcarpiom@users.noreply.github.com>
2024-05-16 14:00:20 -0600

    loops scaffold added

--- lj_param_gen.py
+++ lj_param_gen.py
@@ -15,3 +15,12 @@ for i in lines:
     lj_type.append(line[1])
     epsilon.append(float(line[2]))
     sigma.append(float(line[3]))
+
+print(len(index))
+print(lj_type)
+for i in range(len(index)):
+    for j in range(len(index)):
+        if (lj_type[i] == '12') and (lj_type[j] == '12'):
+            print('same lorentz-berthelot rule')
+        else: 
+            print('different lj type')
